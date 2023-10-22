0f3d2e1c41f42099167d5a71bfb19dedb5ef8e2e
ChingLingTung <littlesmall0540@gmail.com>
2023-10-23 01:46:00 +0800

    修改設施表格的編輯檔案、完成設施表格的刪除檔案、次功能列新增編輯選項

--- DB-php/DB-CRUD/parts/navbar.php
+++ DB-php/DB-CRUD/parts/navbar.php
@@ -26,9 +26,9 @@ $pageName='';
             <li class="nav-item">
               <a class="nav-link <?= $pageName=='add'? 'active':'' ?>" href="./<?= $formName?>_add.php">新增資料</a>
             </li>
-            <!-- <li class="nav-item">
-              <a class="nav-link <?= $pageName=='cate2'? 'active':'' ?>" href="./cate2.php">二層選單</a>
-            </li> -->
+            <li class="nav-item">
+              <a class="nav-link <?= $pageName=='edit'? 'active':'' ?>" href="./<?= $formName?>_edit.php">修改資料</a>
+            </li>
           </ul>
           <ul class="navbar-nav mb-2 mb-lg-0">
 
